80f78304c5a164ecc732068381f1acd84cf83f8a
Ahmad Fauzi <fauzitaufiqurrahman218@gmail.com>
2024-10-09 20:31:07 +0700

    Progress Day 1

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,8 +1,20 @@
-print("My name is" + " " + "Angela")
+# The Variable. (name = variable) So you can call it later on
+# name = input("What is your name?: ")
+# print(name)
 
-# == -- Input -- ==
-# input("Enter your name here: ")
+name_1 = "Anakin"
+name_2 = "Jedi"
+name_3 = "StroomTrooper"
 
-# Take an input for the output
-# print("Greeting " + input("Enter your name here: "))
-print("Greeting " + input("Enter your name here: ") + "!")
+# len() is used for measure string length
+print(len(name_1))
+print(len(name_2))
+print(len(name_3))
+
+# PAUSE 1 - Task
+# print(len(input("📕️ Insert your name here: ")))
+
+# PAUSE 2 - Task
+user = input("📔️ Please enter your name: ")
+length = len(user)
+print(length)
