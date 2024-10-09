e3037215879022d797e9e1a1396d7f7c9733459f
Ahmad Fauzi <fauzitaufiqurrahman218@gmail.com>
2024-10-09 20:37:47 +0700

    Progress Day 1

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,20 +1,7 @@
-# The Variable. (name = variable) So you can call it later on
-# name = input("What is your name?: ")
-# print(name)
-
-name_1 = "Anakin"
-name_2 = "Jedi"
-name_3 = "StroomTrooper"
-
-# len() is used for measure string length
-print(len(name_1))
-print(len(name_2))
-print(len(name_3))
-
-# PAUSE 1 - Task
-# print(len(input("📕️ Insert your name here: ")))
-
-# PAUSE 2 - Task
-user = input("📔️ Please enter your name: ")
-length = len(user)
+name = "Gonzales"
+length = len(name)
 print(length)
+
+username = input("🏗️ Enter your name here: ")
+lengths = len(username)
+print(lengths)
