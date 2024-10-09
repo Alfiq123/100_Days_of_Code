12dab72f622ae6dfb61e06bd78d45f1e2d8676b5
Ahmad Fauzi <fauzitaufiqurrahman218@gmail.com>
2024-10-09 14:24:58 +0700

    Initial commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,9 @@
+# Write your code below this line 👇
+print("Hello World!")
+
+# \n Statement
+print("Hello World1!\nHello World2!")
+print("Hello WorldA!\nHello WorldB!\nHello World3C")
+
+# + Statement
+print("Hello World 1A" + " - " + "Hello World 2A")
