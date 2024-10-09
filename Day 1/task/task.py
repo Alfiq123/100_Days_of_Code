7991f7342145c68bb7bc258cd111bf0a7fa412aa
Ahmad Fauzi <fauzitaufiqurrahman218@gmail.com>
2024-10-09 20:38:20 +0700

    Progress Day 1

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,12 +1,2 @@
-# Methods 1
-print("Welcome to the Band Name Generator.\n❔️ What's the name of the city you grew up in?")
-inputs_1 = input()
-print("❔️ What's your pet's name?")
-inputs_2 = input()
-print("🎷️ Your band name could be: " + inputs_1 + " " + inputs_2)
-
-# Methods 2
-print("Welcome to the Band Name Generator.")
-stage_1 = input("❔️ What's the name of the city you grew up in?: \n")
-stage_2 = input("❔️ What's your pet's name?: \n")
-print("🎷️ Your band name could be " + stage_1 + " " + stage_2)
+# Write your code below this line 👇
+print("Hello World!")
