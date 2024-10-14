3371252996a81c0f541dcbf45d601c3f0ee0d6a0
Ahmad Fauzi <fauzitaufiqurrahman218@gmail.com>
2024-10-14 20:56:44 +0700

    Progress Day 6

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,35 @@
+# def <function>()
+#   Do This
+#   Then Do This
+#   Finally This
+
+def my_function():
+    print("Hello")
+    print("World")
+my_function()
+
+# While Loops
+
+# while stuff_is_true
+#     Do this
+#     Do this, then
+#     Do that
+
+# def squared():
+#     turn_left()
+#     move()
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#     move()
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#     move()
+#     turn_left()
+#
+# while not at_goal():
+#     if wall_in_front():
+#         squared()
+#     else:
+#         move()
